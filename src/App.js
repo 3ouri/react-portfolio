937bddc859693094a69a2d27004f704a4614ac01
Abdallah Mustafa <abdallah.ouri@hotmail.com>
2022-11-20 15:04:17 -0600

    Services Completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { particlesOptions } from "./particlesConfig";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
+import Services from "./components/Services";
 
 function App() {
   const particlesInit = (engine) => {
@@ -18,6 +19,7 @@ function App() {
       <Particles className="tsparticles" init={particlesInit} options={particlesOptions} />
       <Header />
       <AboutMe />
+      <Services />
     </>
   );
 }
